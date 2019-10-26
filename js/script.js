a3b21aadfa13a4c8ec382e13062c09fb99c4cb75
didadadida93 <didadadida93@gmail.com>
2019-10-26 09:55:43 +0700

    update webpage

--- js/script.js
+++ js/script.js
@@ -16,6 +16,7 @@ $(document).ready(() => {
         if ($(".right-panel-content").is(":visible")) {
           $(".right-panel-content").fadeOut(300, () => {
             $("#project").fadeIn(300)
+            $("#project")[0].scrollIntoView()
           })
         } else {
           let section = [...$("section")]
@@ -24,11 +25,13 @@ $(document).ready(() => {
 
           $(`#${section[0].id}`).fadeOut(300, () => {
             $("#project").fadeIn(300)
+            $("#project")[0].scrollIntoView()
           })
         }
       } else {
         $(".right-panel-content").fadeOut(300, () => {
           $(e.currentTarget.attributes.href.value).fadeIn(300)
+          $(e.currentTarget.attributes.href.value)[0].scrollIntoView()
         })
       }
     }
@@ -40,20 +43,21 @@ $(document).ready(() => {
 
     // first check all section and make it disappear
     let sections = [...$("section")].filter(section => section.className === "posting")
-    // sections = sections.filter(section => section.className === "posting")
     sections = [...sections, ...$("#project")]
     sections.forEach(section => {
       if ($(`#${section.id}`).is(":visible")) $(`#${section.id}`).fadeOut(300)
     })
 
     // then check where this button got clicked and make it disappear
-    if ($(`#${e.currentTarget.attributes.target.value}`).is(":visible")) {
-      $(`#${e.currentTarget.attributes.target.value}`).fadeOut(300, () => {
+    if ($(e.currentTarget.attributes.target.value).is(":visible")) {
+      $(e.currentTarget.attributes.target.value).fadeOut(300, () => {
         $(".right-panel-content").fadeIn(300)
+        $("#right-panel .right-panel-content h1")[0].scrollIntoView()
       })
     } else {
       // this element already disappear
       $(".right-panel-content").fadeIn(300)
+      $("#right-panel .right-panel-content h1")[0].scrollIntoView()
     }
   })
 })
